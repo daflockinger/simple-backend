3581316d0e0532bd1aee1aa125743aee152f1dd6
daflockinger <florian.mitterbauer@gmx.net>
2023-06-04 10:14:22 +0200

    created base project

---
+++ src/test/kotlin/com/flockinger/simplebackend/SimpleBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.flockinger.simplebackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SimpleBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
